462a9d5ed320ccfdced35494018c02778fb9fd45
hermwong <herm.wong@gmail.com>
2012-03-29 17:03:08 -0700

    added network connection api

--- framework/cordova-1.6.0.js
+++ framework/cordova-1.6.0.js
@@ -1369,17 +1369,35 @@ Network.prototype.isReachable = function(hostName, successCallback, options) {
 	    method: 'getstatus',
 	    parameters: {},
 	    onSuccess: function(result) { 
+		
 			var status = NetworkStatus.NOT_REACHABLE;
-			if (result.isInternetConnectionAvailable == true)
-			{
-				// don't know whether its via wifi or carrier ... so return the worst case
-				status = NetworkStatus.REACHABLE_VIA_CARRIER_DATA_NETWORK;
+			if (result.isInternetConnectionAvailable == true) {
+
+				if (result.wan.state == "connected") {
+					status = NetworkStatus.REACHABLE_VIA_CARRIER_DATA_NETWORK;
+				}
+				
+				if (result.wifi.state == "connected") {
+					status = NetworkStatus.REACHABLE_VIA_WIFI_NETWORK;
+				}
+							
 			}
 			successCallback(status); 
 		},
 	    onFailure: function() {}
 	});
 
+};
+
+Network.prototype.connection = function(hostName, successCallback, options) {
+	this.request = navigator.service.Request('palm://com.palm.connectionmanager', {
+	    method: 'getstatus',
+	    parameters: {},
+	    onSuccess: function(result) { 
+			successCallback(result); 
+		},
+	    onFailure: function() {}
+	});	
 };
 
 /*
