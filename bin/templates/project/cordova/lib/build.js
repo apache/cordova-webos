368e8f4fc00390bc9512c28faecf17a55c432a69
Jason Robitaille <robitaille88@gmail.com>
2014-05-18 02:17:13 -0500

    Added project-included build/run/version scripts and related modules

---
+++ bin/templates/project/cordova/lib/build.js
@@ -0,0 +1,81 @@
+#!/usr/bin/env node
+
+/*
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+*/
+
+var shell = require("shelljs"),
+    spawn = require("./ares-spawn"),
+    Q = require("q"),
+    path = require("path"),
+    fs = require("fs"),
+    ROOT = path.join(__dirname, "..", "..");
+
+
+/*
+ * Builds the project with ares-package.
+ * Returns a promise.
+ */
+module.exports.run = function(build_type) {
+    build_type = typeof build_type !== "undefined" ? build_type : "";
+    var binDir = path.join(ROOT, "bin");
+    if(!fs.existsSync(binDir)) {
+        shell.mkdir("-p", binDir);
+    }
+    var args = ["-o", path.join(ROOT, "bin")];
+    if(build_type==="--debug" || build_type==="--no-minify") {
+        args.push("-n");
+    }
+    args.push(path.join(ROOT, "www"));
+    return spawn("ares-package", args);
+}
+
+/*
+ * Gets the path to the ipk file, if not such file exists then
+ * the script will error out.
+ */
+module.exports.get_ipk = function() {
+    var binDir = path.join(ROOT, "bin");
+    if (fs.existsSync(binDir)) {
+        var candidates = fs.readdirSync(binDir).filter(function(p) {
+            // Need to find most recent .ipk.
+            return path.extname(p) == ".ipk";
+        }).map(function(p) {
+            p = path.join(binDir, p);
+            return { p: p, t: fs.statSync(p).mtime };
+        }).sort(function(a,b) {
+            return a.t > b.t ? -1 :
+                   a.t < b.t ? 1 : 0;
+        });
+        if (candidates.length === 0) {
+            console.error("ERROR : No .ipk found in " + binDir + " directory");
+            process.exit(2);
+        }
+        console.log("Using ipk: " + candidates[0].p);
+        return candidates[0].p;
+    } else {
+        console.error("ERROR : unable to find project " + binDir + " directory, could not locate .ipk");
+        process.exit(2);
+    }
+}
+
+module.exports.help = function() {
+    console.log("Usage: " + path.relative(process.cwd(), path.join(ROOT, 'cordova', 'build')) + " [options]");
+    console.log("    '--debug|--no-minify': Will build the project without minification phase");
+    process.exit(0);
+}
---
+++ bin/templates/project/cordova/lib/run.js
@@ -0,0 +1,90 @@
+#!/usr/bin/env node
+
+/*
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+*/
+
+var path  = require("path"),
+    fs = require("fs"),
+    build = require("./build"),
+    spawn = require("./ares-spawn"),
+    Q = require("q"),
+    ROOT = path.join(__dirname, "..", "..");
+
+/*
+ * Runs the application
+ * Returns a promise.
+ */
+ module.exports.run = function(args) {
+    var build_type;
+    var install_target;
+
+    for (var i=2; i<args.length; i++) {
+        if (args[i] == "--debug" || args[i] == "--no-minify") {
+            build_type = "--debug";
+        } else if (args[i] == "--emulator") {
+            install_target = "emulator";
+        } else if (args[i].substring(0, 9) == "--target=") {
+            install_target = args[i].substring(9, args[i].length);
+        } else {
+            console.error("ERROR : Run option \"" + args[i] + "\" not recognized.");
+            process.exit(2);
+        }
+    }
+
+    return build.run(build_type).then(function() {
+        var installArgs = [];
+        if(install_target) {
+            installArgs.push("-d", install_target);
+        }
+        installArgs.push(build.get_ipk());
+        return spawn("ares-install", installArgs).then(function() {
+            var appinfoPath = path.join(ROOT, "www", "appinfo.json");
+            if(fs.existsSync(appinfoPath)) {
+                try {
+                    var appinfo = JSON.parse(fs.readFileSync(appinfoPath));
+                    return module.exports.launch(appinfo.id);
+                } catch(e) {
+                    console.error("ERROR : Unable to read \"appinfo.json\"; cannot determine appID to launch.");
+                    process.exit(2);
+                }
+              }
+        });
+    });
+}
+
+/*
+ * Runs the application
+ * Returns a promise.
+ */
+module.exports.launch = function(appID) {
+  var doLaunch = function() {
+      return spawn("ares-launch", [appID]);
+  };
+  // close application if it's open, then launch
+  return spawn("ares-launch", ["-c", appID], "ignore").then(doLaunch, doLaunch);
+}
+
+module.exports.help = function() {
+    console.log("Usage: " + path.relative(process.cwd(), path.join(ROOT, 'cordova', 'run')) + " [options]");
+    console.log("Build options :");
+    console.log("    --debug|--no-minify : Builds project without minification");
+    console.log("    --emulator : Will deploy the built project to an emulator if one exists");
+    console.log("    --target=<target_id> : Installs to the target with the specified id.");
+    process.exit(0);
+}
