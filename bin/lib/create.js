e5c7c43d23f460ece7f32b50021a3ada12c9093c
Suraj Pindoria <suraj.pindoria@yahoo.com>
2015-06-25 11:02:59 -0700

    CB-8965 Copy cordova-js-src directory to platform folder during create. This closes #4

--- bin/lib/create.js
+++ bin/lib/create.js
@@ -65,6 +65,9 @@ exports.createProject = function(project_path,project_id,project_name){
             //copy cordova js file
             shjs.cp(path.join(ROOT, 'cordova-lib', 'cordova.js'), path.join(project_path,'www'));  
 
+            //copy cordova-js-src directory
+            shjs.cp('-rf', path.join(ROOT, 'cordova-js-src'), path.join('project_path', 'platform_www'));
+
             //copy cordova folder
             shjs.cp('-r', path.join(ROOT, 'bin', 'templates', 'project', 'cordova'), project_path);
             shjs.cp('-r', path.join(ROOT, 'node_modules'), path.join(project_path,'cordova'));
